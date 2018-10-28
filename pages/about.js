3ff127839e2bd8ad45e4ac9841e4c679f7625725
zee-german <franz.lutzenberger@gmail.com>
2018-10-28 09:48:18 +0100

    about and index pages added

---
+++ pages/about.js
@@ -0,0 +1,9 @@
+const About = () => (
+  <section>
+    <h1>About NextJS</h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perspiciatis porro et, facilis, similique beatae, est quod officiis itaque libero delectus alias. Repudiandae quis, exercitationem ducimus. Officiis qui odio non soluta excepturi, vel dolorem quae saepe neque porro, eum eius voluptatum enim nulla itaque iusto explicabo dolorum dolor tempora sint velit.
+    </p>
+  </section>
+)
+
+export default About
---
+++ pages/index.js
@@ -0,0 +1,10 @@
+import Link from 'next/link'
+
+const Index = () => (
+  <section>
+  <h1>Hello world from next.js</h1>
+  <Link href="/about"><a title="About NextJS">About this Project</a></Link>
+  </section>
+)
+
+export default Index
