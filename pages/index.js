51eedae20e5c17ff6629e8bc015a8666b527c1a2
zee-german <franz.lutzenberger@gmail.com>
2018-10-28 11:27:20 +0100

    added page title to index page

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,12 @@
 import Navbar from '../components/Navbar';
+import Head from 'next/head';
 import '../scss/style.scss';
 
 const Index = () => (
   <section>
+    <Head>
+      <title>[I]ndex</title>
+    </Head>
     <Navbar />
     <h1>Hello world from next.js</h1>
   </section>
